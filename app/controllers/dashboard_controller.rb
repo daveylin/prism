1babc4026eecd75ae33cca41cbcb8a6011bb070c
David Lin <daveylin@gmail.com>
2015-09-01 17:23:12 +0000

    search redirects to user index instead of showing results

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,12 @@
 class DashboardController < ApplicationController
   def index
+    #@people = Person.all
+  if params[:search]
+    @people = Person.search(params[:search]).order("first_name ASC")
+    redirect_to dashboard_index_path
+  else
+    #@people = Person.all.order("first_name ASC")
+  end
     authorize current_user
   end
   
