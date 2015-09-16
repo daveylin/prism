1c3829a69e4ef18a412da99e6a8ea1bbb8e325f5
David Lin <daveylin@gmail.com>
2015-09-16 04:59:57 +0000

    hotline show working, testing model

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -45,6 +45,7 @@ class PeopleController < ApplicationController
 
   def show
     @person = Person.find(params[:id])
+    @hotlines = Hotline.where(person_id: @person.id)
   end
   
   private
--- app/models/hotline.rb
+++ app/models/hotline.rb
@@ -2,6 +2,7 @@ class Hotline
   include Mongoid::Document
   #embedded_in :person
   
+  field :person_id, type: Integer
   field :city, type: String
   field :zip, type: String
   field :client, type: Boolean
